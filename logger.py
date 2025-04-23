e4ec267315eee7fbfbc2f1830d517ec234c929d2
Seckin Adali <adaliseckin@gmail.com>
2025-04-23 11:58:53 +0300

    Update logger.py to have separate file/console handlers with debug/info levels

--- logger.py
+++ logger.py
@@ -10,13 +10,18 @@ logs_dir = Path(__file__).resolve().parent / "logs"
 if not logs_dir.exists():
     logs_dir.mkdir(parents=True)
 
+# More detailed logs in file
+file_handler = logging.FileHandler(logs_dir / "pipeline.log")
+file_handler.setLevel(logging.DEBUG)
+
+# Less verbose in console
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.INFO)
+
 # Configure logging
 logging.basicConfig(
-    level=logging.INFO,
+    level=logging.DEBUG,
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     datefmt='%Y-%m-%d %H:%M:%S',
-    handlers=[
-        logging.FileHandler(logs_dir / "pipeline.log"),
-        logging.StreamHandler()
-    ]
+    handlers=[file_handler, console_handler]
 )
